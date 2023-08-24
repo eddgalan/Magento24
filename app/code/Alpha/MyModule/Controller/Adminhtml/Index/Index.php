cca649a4e514aa0e57d4f1db0f54461f3794018f
Edson Galan <edsongalan@wolfsellers.com>
2023-08-24 12:47:11 -0600

    Agrega ACL - Agrega el acl.xml para administrar los recursos (resource) - Agrega el método _isAllowed() para controlar el 'resource' agregado

--- app/code/Alpha/MyModule/Controller/Adminhtml/Index/Index.php
+++ app/code/Alpha/MyModule/Controller/Adminhtml/Index/Index.php
@@ -10,16 +10,31 @@ class Index extends Action
 {
     protected $resultPageFactory;
 
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+
     public function __construct(Context $context, PageFactory $resultPageFactory)
     {
         parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
     }
 
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|\Magento\Framework\View\Result\Page
+     */
     public function execute()
     {
-        $resultPage = $this->resultPageFactory->create();
-        return $resultPage;
+        return $this->resultPageFactory->create();
     }
 
+    /**
+     * @return bool
+     */
+    public function _isAllowed(): bool
+    {
+        return $this->_authorization->isAllowed('Alpha_MyModule::index');
+        // return parent::_isAllowed('Alpha_MyModule::index');
+    }
 }
